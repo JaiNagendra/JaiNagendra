bdc113e003a73d580a6ced487ab443c924f76c53
JaiNagendra <jainagendra2004@gmail.com>
2025-06-12 08:59:56 +0530

    Fix or update SocketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-app-production2.onrender.com/", {
+			const socket = io("https://chat-app-production2.onrender.com/~~~", {
 				query: {
 					userId: authUser._id,
 				},
